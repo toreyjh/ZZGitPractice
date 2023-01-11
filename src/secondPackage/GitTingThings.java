e4716629479188edf5c302c49d8c7560a78cc452
Torey <Torey@TJH-DI5485>
2023-01-10 22:02:52 -0500

    trying

--- src/secondPackage/GitTingThings.java
+++ src/secondPackage/GitTingThings.java
@@ -1,11 +1,13 @@
 package secondPackage;
 
-public class GetTingThings {
+public class GitTingThings {
 
 	public static void main(String[] args) {
 		
 		
 		System.out.println("this is a sample to git  practice with git");
 	}
+	
+	
 }
                       
