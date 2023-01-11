3085f38e8a193f6788dd1c4ae3f6ab5ce76f43c3
Torey <Torey@TJH-DI5485>
2023-01-10 21:01:18 -0500

    sending tings

--- src/secondPackage/GetTingThings.java
+++ src/secondPackage/GetTingThings.java
@@ -5,6 +5,7 @@ public class GetTingThings {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("thie is a sample to git  practice with git");
+		System.out.println("this is a sample to git  practice with git");
 	}
 }
+main                        
