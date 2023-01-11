3a4ee78b7c8cf91d0a967471d1e6a878dd02d1db
Torey <Torey@TJH-DI5485>
2023-01-10 20:47:58 -0500

    message

---
+++ src/secondPackage/GetTingThings.java
@@ -0,0 +1,10 @@
+package secondPackage;
+
+public class GetTingThings {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("thie is a sample to git  practice with git");
+	}
+}
